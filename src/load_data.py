4eaa81e6d390bccd05b9ca3ec333f359cc02cc87
Marco Chiarandini <marco@imada.sdu.dk>
2021-09-13 18:24:41 +0200

    added data zhiru-ds

--- src/load_data.py
+++ src/load_data.py
@@ -42,6 +42,118 @@ class Problem:
 
     
 
+   
+    def check_tot_capacity(self):
+        capacity = sum([self.project_details[k]["max_cap"] for k in self.project_details])
+        n_stds = len(self.student_details)
+        if (capacity < n_stds):
+            answer = input(
+                "Not enough capacity from all projects\nHandle this by including a dummy project with the needed capacity? (y/n)\n")
+            if answer in ['Y', 'y']:
+                sys.exit("to implement")
+                # file.write(str(len(project_dict)+1)+";;1;"+str(n_stds-capacity)+";"+program+"\n")
+                #project_dict[len(project_dict)+1] = n_stds-capacity
+
+    def read_students(self, dirname):
+        #students_file = dirname+"/students.csv"
+        #dtypes_file = dirname+"/dtypes.csv"
+        #print("read ", students_file)
+        data_file = dirname+"/data.xlsx"
+        features_orddict = OrderedDict()
+        student_table = pd.DataFrame()
+        try:
+            with open(data_file, 'rb') as f:
+                features_df = pd.read_excel(f, sheet_name='dtypes', header=0, index_col=None)
+                # for x in f:
+                #    row = x.split(";")
+                #student_dtypes[row[0]] = row[1].strip()
+                # print(dtypes)
+                features_orddict = features_df.to_dict("index", into=OrderedDict)
+                # print(student_dtypes)
+                # dtypes.to_dict("index",into=OrderedDict))
+                # dtypes = {'grp_id': 'str', 'group': 'str', 'username': 'str', 'type': 'str', 'email': 'str', 'student_id': 'str',
+                #          'full_name': 'str', 'priority_list': 'str'}
+                dtypes = {'grp_id': 'str', 'username': 'str', 'type': 'str', 'priority_list': 'str'}
+                dtypes.update({row['Variable']: row['Type']
+                               for index, row in features_df.iterrows()})
+                print(dtypes)
+                student_table = pd.read_excel(f, sheet_name="students", header=0, index_col=None,
+                                              dtype=dtypes, keep_default_na=False) #, decimal=',')
+                student_table["username"]=student_table["username"].apply(lambda x: x.lower())
+        except FileNotFoundError:
+            print("No file 'data.xlsx' found")
+        print(student_table)
+        counters = student_table.groupby(['type']).size().reset_index(name='counts')
+        print(counters)
+        # grp_id;group;username;type;priority_list;student_id;full_name;email;timestamp
+        # student_table = pd.read_csv(dirname+"/students.csv", sep=";", dtype=student_dtypes, keep_default_na=False, decimal=',')
+        # student_table["username"].apply(lambda x: x.lower())
+        # print(student_table)
+
+        # print(student_table.dtypes)
+        # Transform the categorical values in integers
+        categories = OrderedDict()
+        # for f in student_table.columns:
+        for feat in features_orddict:
+            f = features_orddict[feat]['Variable']
+            if student_table[f].dtype.name == 'category':
+                # print(student_table[f].cat.categories,len(student_table[f].cat.categories))
+                student_table[f+"_rcat"] = student_table[f].cat.rename_categories(
+                    range(len(student_table[f].cat.categories)))
+                categories[f+"_rcat"] = {x: i for (i, x)
+                                        in enumerate(student_table[f+"_rcat"].cat.categories)}
+
+        # print(student_table.dtypes)
+        #print(student_table.iloc[:, range(9, student_table.shape[1])])
+        print(student_table.loc[:, dtypes.keys()]) # range(9, student_table.shape[1])])
+
+        student_table.index = student_table["username"]
+        student_details = student_table.to_dict("index", into=OrderedDict)
+
+        for s in student_details:
+            student_details[s]["priority_list"] = [list(map(int, x.strip().split(" "))) for x in student_details[s]["priority_list"].split(
+                ",")] if student_details[s]["priority_list"] != '' else []
+        # print(student_details)
+        #reader = csv.reader(open(students_file, "r", encoding="utf8"), delimiter=";")
+
+        # student_details = {}
+        # # GruppeId; Brugernavn; StudType; Prioteringsliste; Studentnavn;  Email; Tilmeldingstidspunkt
+        # for line in reader:
+        #     if line[0][0] == "#":
+        #         continue
+        #     username = line[1].lower()
+        #     student_details[username] = dict(
+        #         GruppeID=line[0],
+        #         Brugernavn=username,
+        #         StudType=line[2].lower(),
+        #         # Studieretning=line[3].lower(),
+        #         PrioriteringsListe=[int(x) for x in line[3].split(",")],
+        #         # CprNr=(len(line)>4 and line[4] or ""),
+        #         # Fornavne=(len(parts)>4 and parts[5] or ""),
+        #         # Efternavn=(len(parts)>4 and parts[6] or ""),
+        #         Navn=(len(line) > 4 and line[4] or ""),
+        #         Email=(len(line) > 4 and line[5] or ""),
+        #         Tilmeldingstidspunkt=(len(line) > 4 and line[6] or "")
+        #     )
+
+        filehandle = codecs.open(os.path.join("log", "students.json"),  "w", "utf-8")
+        json.dump(student_details, fp=filehandle, sort_keys=True,
+                  indent=4, separators=(',', ': '),  ensure_ascii=False)
+
+        priorities = {u: student_details[u]["priority_list"] for u in student_details}
+        tmp = {u: (student_details[u]["grp_id"], student_details[u]["type"])
+               for u in student_details}
+        group_ids = {student_details[u]["grp_id"] for u in student_details}
+        groups = {g: list(
+            filter(lambda u: student_details[u]["grp_id"] == g, student_details.keys())) for g in group_ids}
+
+        student_types = {student_details[u]["type"] for u in student_details}
+        print(student_types)
+        std_type = {u: student_details[u]["type"] for u in student_details}
+        print(std_type)
+        return (student_details, features_orddict, categories, priorities, groups, std_type)
+        
+
     def read_projects(self, dirname):
         #projects_file = dirname+"/projects.csv"
         #print("read ", projects_file)
@@ -96,113 +208,8 @@ class Problem:
                                        )
         return (project_details, topics, projects)
 
-    def check_tot_capacity(self):
-        capacity = sum([self.project_details[k]["max_cap"] for k in self.project_details])
-        n_stds = len(self.student_details)
-        if (capacity < n_stds):
-            answer = input(
-                "Not enough capacity from all projects\nHandle this by including a dummy project with the needed capacity? (y/n)\n")
-            if answer in ['Y', 'y']:
-                sys.exit("to implement")
-                # file.write(str(len(project_dict)+1)+";;1;"+str(n_stds-capacity)+";"+program+"\n")
-                #project_dict[len(project_dict)+1] = n_stds-capacity
 
-    def read_students(self, dirname):
-        #students_file = dirname+"/students.csv"
-        #dtypes_file = dirname+"/dtypes.csv"
-        #print("read ", students_file)
-        data_file = dirname+"/data.xlsx"
-        features_orddict = OrderedDict()
-        student_table = pd.DataFrame()
-        try:
-            with open(data_file, 'rb') as f:
-                features_df = pd.read_excel(f, sheet_name='dtypes', header=0, index_col=None)
-                # for x in f:
-                #    row = x.split(";")
-                #student_dtypes[row[0]] = row[1].strip()
-                # print(dtypes)
-                features_orddict = features_df.to_dict("index", into=OrderedDict)
-                # print(student_dtypes)
-                # dtypes.to_dict("index",into=OrderedDict))
-                dtypes = {'grp_id': 'str', 'group': 'str', 'username': 'str', 'type': 'str', 'email': 'str', 'student_id': 'str',
-                          'full_name': 'str', 'priority_list': 'str'}
-                dtypes.update({row['Variable']: row['Type']
-                               for index, row in features_df.iterrows()})
-                print(dtypes)
-                student_table = pd.read_excel(f, sheet_name="students", header=0, index_col=None,
-                                              dtype=dtypes, keep_default_na=False) #, decimal=',')
-                student_table["username"]=student_table["username"].apply(lambda x: x.lower())
-        except FileNotFoundError:
-            print("No file 'data.xlsx' found")
-        print(student_table)
-        counters = student_table.groupby(['type']).size().reset_index(name='counts')
-        print(counters)
-        # grp_id;group;username;type;priority_list;student_id;full_name;email;timestamp
-        # student_table = pd.read_csv(dirname+"/students.csv", sep=";", dtype=student_dtypes, keep_default_na=False, decimal=',')
-        # student_table["username"].apply(lambda x: x.lower())
-        # print(student_table)
 
-        # print(student_table.dtypes)
-        # Transform the categorical values in integers
-        categories = OrderedDict()
-        # for f in student_table.columns:
-        for feat in features_orddict:
-            f = features_orddict[feat]['Variable']
-            if student_table[f].dtype.name == 'category':
-                # print(student_table[f].cat.categories,len(student_table[f].cat.categories))
-                student_table[f+"_rcat"] = student_table[f].cat.rename_categories(
-                    range(len(student_table[f].cat.categories)))
-                categories[f+"_rcat"] = {x: i for (i, x)
-                                        in enumerate(student_table[f+"_rcat"].cat.categories)}
-
-        # print(student_table.dtypes)
-        print(student_table.iloc[:, range(9, student_table.shape[1])])
-
-        student_table.index = student_table["username"]
-        student_details = student_table.to_dict("index", into=OrderedDict)
-
-        for s in student_details:
-            student_details[s]["priority_list"] = [list(map(int, x.strip().split(" "))) for x in student_details[s]["priority_list"].split(
-                ",")] if student_details[s]["priority_list"] != '' else []
-        # print(student_details)
-        #reader = csv.reader(open(students_file, "r", encoding="utf8"), delimiter=";")
-
-        # student_details = {}
-        # # GruppeId; Brugernavn; StudType; Prioteringsliste; Studentnavn;  Email; Tilmeldingstidspunkt
-        # for line in reader:
-        #     if line[0][0] == "#":
-        #         continue
-        #     username = line[1].lower()
-        #     student_details[username] = dict(
-        #         GruppeID=line[0],
-        #         Brugernavn=username,
-        #         StudType=line[2].lower(),
-        #         # Studieretning=line[3].lower(),
-        #         PrioriteringsListe=[int(x) for x in line[3].split(",")],
-        #         # CprNr=(len(line)>4 and line[4] or ""),
-        #         # Fornavne=(len(parts)>4 and parts[5] or ""),
-        #         # Efternavn=(len(parts)>4 and parts[6] or ""),
-        #         Navn=(len(line) > 4 and line[4] or ""),
-        #         Email=(len(line) > 4 and line[5] or ""),
-        #         Tilmeldingstidspunkt=(len(line) > 4 and line[6] or "")
-        #     )
-
-        filehandle = codecs.open(os.path.join("log", "students.json"),  "w", "utf-8")
-        json.dump(student_details, fp=filehandle, sort_keys=True,
-                  indent=4, separators=(',', ': '),  ensure_ascii=False)
-
-        priorities = {u: student_details[u]["priority_list"] for u in student_details}
-        tmp = {u: (student_details[u]["grp_id"], student_details[u]["type"])
-               for u in student_details}
-        group_ids = {student_details[u]["grp_id"] for u in student_details}
-        groups = {g: list(
-            filter(lambda u: student_details[u]["grp_id"] == g, student_details.keys())) for g in group_ids}
-
-        student_types = {student_details[u]["type"] for u in student_details}
-        print(student_types)
-        std_type = {u: student_details[u]["type"] for u in student_details}
-        print(std_type)
-        return (student_details, features_orddict, categories, priorities, groups, std_type)
 
     def calculate_ranks_values(self, prioritize_all=False):
         std_values = {}
