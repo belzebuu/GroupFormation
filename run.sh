5923c05af66c2b86982cad157148cf0c4e68aea0
Marco Chiarandini <marco@imada.sdu.dk>
2024-08-28 22:35:53 +0200

    run

--- run.sh
+++ run.sh
@@ -5,5 +5,6 @@
 for i in zhiru-f20  zhiru-e21  zhiru-e21-ds  zhiru-e22-ds  zhiru-e22-ds830  zhiru-e23-ds831;
     do 
         echo $i;
-        python3 src/prensio/main.py -t 3600 -s patat/${i}_sol -L patat/${i}_log -x data/private/$i;
+	python3 src/prensio/main.py -t 3600 -s patat/${i}_sol -L patat/${i}_log -x data/private/$i;
+	mv prensio.log patat/${i}_log
     done
